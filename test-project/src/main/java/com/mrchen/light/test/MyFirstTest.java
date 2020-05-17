00f9fbd54d39902b50425c372927fa4a9448acc3
陈帅 <chen_shuai@sinosoft.com.cn>
2020-05-17 17:51:18 +0800

    测试类增加描述

--- test-project/src/main/java/com/mrchen/light/test/MyFirstTest.java
+++ test-project/src/main/java/com/mrchen/light/test/MyFirstTest.java
@@ -5,7 +5,9 @@ import net.sf.json.JSONObject;
 
 /**
  * @program: light-framework-custom
- * @description:
+ * @description: 运行测试类 MyRequestClient为网络申请的类 ， TestMethodHandler为 具体网络申请的实现类 可自己写
+ *                最后运行结果为：
+ *                JSON:   {"errcode":40013,"errmsg":"invalid corpid, hint: [1589708713_50_790b28916c092728bcc8d59b193b1b17], from ip: 111.227.254.169, more info at https://open.work.weixin.qq.com/devtool/query?e=40013"}
  * @author: mrchen
  * @create: 2020-05-09 15:12
  */
@@ -15,4 +17,5 @@ public class MyFirstTest {
          JSONObject jsonObject= myRequestClient.getAccess_token("111","222");
          System.out.println("JSON:   "+jsonObject);
     }
+
 }
